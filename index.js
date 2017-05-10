02dce79296cf2815f44b09bfe7f9c2a90c641ef6
zhanghao639 <zhanghao639@pingan.com.cn>
2017-05-10 20:45:29 +0800

    Revert "修复css直接生成到style里，无法正常拿到domain的问题"
    This reverts commit a390e205081d216ec717ac8b49fa6409494c0a4c.

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
 'use strict';
 
 var REG = /<(script|style|template)>([\s\S]*)<\/\1>/gi;
+var CSS_TPL = feather.util.read(__dirname + '/css.tpl');
 
 module.exports = function(content, file){
     var script = '', tpl = '', style = '';
 
     content.toString().replace(REG, function(all, tag, cont){
-        if(cont.trim() == '') return;
-
         if(tag == 'script'){
             script = cont;
         }else if(tag == 'template'){
             tpl = cont;
         }else{
+            if(cont.trim() == '') return;
+
             //生成一个临时文件进行css编译
-            var css = feather.file.wrap(feather.project.getProjectPath() + file.subpathNoExt + '_.css');
+            var css = feather.file.wrap(feather.project.getProjectPath() + file.subpathNoExt + '.css');
             css.setContent(cont);
+            css.release = false;
+            css.useMap = false;
             feather.compile(css);
-            file.addRequire(css.id);
+            style = CSS_TPL.replace('__CSS__', JSON.stringify(css.getContent()));
         }
     });
 
-    if(!script && !tpl){
-        return content;
-    }
-
-    if(tpl){
+    if(tpl.trim() != ''){
         //exports.default 支持后续es6编译
         script += ';var _vueTpl = ' + JSON.stringify(tpl) + ';module.exports[\'default\'] ? (module.exports[\'default\'].template = _vueTpl)'
         + ' : (module.exports.template = _vueTpl);';
+    }
+
+    if(style.trim() != ''){
+        script += style;
     }
 
     return script;
